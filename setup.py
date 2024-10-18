763605d8f202896aeee96f305a32c0db162bc442
Bryan Alexander Pinzon Macedo <bryanpinzon469@gmail.com>
2024-10-18 12:47:06 -0500

    f

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ README = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name='peruscp',
-    version='0.0.2.2',
+    version='0.0.2.3',
     license='MIT',
     description='Es un paquete que realiza una solicitud a eldni.com extrayendo todos los datos a consultar, util para bots, APIS, Paginas WEB, son datos basicos de Reniec.',
     long_description=README,
     long_description_content_type="text/markdown",
     author='bapinzon',
     packages=find_packages(),
-    install_requires=['requests','BeautifulSoup'],
+    install_requires=['requests','beautifulsoup4'],
     author_email="bryanpinzon469@gmail.com",
     url='https://github.com/bapinzon/peruscp'
 )
