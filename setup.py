ee4d26c5fa31ceac3147266373bc434a6123dca7
Bryan Alexander Pinzon Macedo <bryanpinzon469@gmail.com>
2024-10-18 12:33:11 -0500

    basico

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
     version='0.0.2',
     license='MIT',
     description='Es un paquete que realiza una solicitud a eldni.com extrayendo todos los datos a consultar, util para bots, APIS, Paginas WEB, son datos basicos de Reniec.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     author='bapinzon',
     packages=find_packages(),
     install_requires=['requests','BeautifulSoup'],
